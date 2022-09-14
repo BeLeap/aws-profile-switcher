602ec9ffa2ed7265e0f43156925031599d89ae79
Changseo <changseo.jang@riiid.co>
2022-09-14 18:03:45 +0900

    load aws profiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    let home_dir = home::home_dir().unwrap();
+    let aws_config_content = fs::read_to_string(format!("{}/.aws/config", home_dir.into_os_string().into_string().unwrap())).unwrap();
+
+    let profiles: Vec<&str> = aws_config_content.lines().fold(vec![], |acc, line| {
+        if line.starts_with("[") && line.ends_with("]") {
+            [acc, vec![line]].concat()
+        } else {
+            acc
+        }
+    });
+
+    println!("{:?}", profiles);
 }
