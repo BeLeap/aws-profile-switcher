da0ec7063709cd5915878a70fd962f44b0b45682
Changseo <changseo.jang@riiid.co>
2022-09-14 18:13:12 +0900

    fuzzy find profiles

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use std::fs;
 
+use fuzzy_finder::item::Item;
+
 
 fn main() {
     let home_dir = home::home_dir().unwrap();
     let aws_config_content = fs::read_to_string(format!("{}/.aws/config", home_dir.into_os_string().into_string().unwrap())).unwrap();
 
-    let profiles: Vec<&str> = aws_config_content.lines().fold(vec![], |acc, line| {
+    let profiles: Vec<Item<&str>> = aws_config_content.lines().fold(vec![], |acc, line| {
         if line.starts_with("[") && line.ends_with("]") {
-            [acc, vec![line]].concat()
+            let profile_name = line.trim_start_matches("[").trim_start_matches("profile ").trim_end_matches("]");
+            [acc, vec![Item::new(profile_name.to_string(), profile_name)]].concat()
         } else {
             acc
         }
     });
+    let profile_count = profiles.len();
 
-    println!("{:?}", profiles);
+    let result = fuzzy_finder::FuzzyFinder::find(profiles, profile_count.try_into().unwrap()).unwrap().unwrap();
+
+    println!("{}", result);
 }
